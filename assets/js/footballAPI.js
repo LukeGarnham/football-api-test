d7820c0b306c20109f39f09f5d6c0e3e0d916f5f
LukeGarnham <lukegarnham@msn.com>
2020-10-16 16:59:54 +0000

    Worked on API a bit more.

--- assets/js/footballAPI.js
+++ assets/js/footballAPI.js
@@ -1,9 +1,9 @@
-function declare(cb) {
+function declare(teamName, cb) {
     var xhr = new XMLHttpRequest();
 
     xhr.withCredentials = true;
 
-    xhr.open("GET", "https://api-football-v1.p.rapidapi.com/v2/teams/league/524");
+    xhr.open("GET", "https://api-football-v1.p.rapidapi.com/v2/teams/search/" + teamName);
     xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
     xhr.setRequestHeader("x-rapidapi-key", "03db490835mshd5eaf0436ca3429p1687b6jsnf4663d1db28c");
     xhr.send();
@@ -15,21 +15,27 @@ function declare(cb) {
     };
 };
 
-// function execute(data) {
-//     console.log(data);
-// };
-
-// declare(execute);
-
 function printToDoc() {
-    declare(function(data) {
-        let content = document.getElementById("teams");
-        content.innerHTML = "";
-        // console.dir(data);
-        teams = data.api.teams;
-        teams.forEach(function(item) {
-            content.innerHTML += item.name;
-        });
+    let searchString = document.getElementById('teamSearch').value;
+    searchString = searchString.replace(/ /g, "_").toLowerCase();
+    declare(searchString, function(data) {
+        let results = document.getElementById("teams");
+        results.innerHTML = "";
+        console.dir(data);
+
+        if (data.api.results == 0) {
+            results.innerHTML = "No teams found.  Please check the spelling or try a different team name.";
+        } else {
+            teams = data.api.teams;
+            teams.forEach(function(item) {
+                console.log(item);
+                results.innerHTML += `<p>${item.name}</p>`;
+                results.onclick = function showClubDetails(teams){
+                    console.log(teams);
+                };
+            });
+        }
+        
         
     });
 };
